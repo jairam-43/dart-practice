0e03041e4cf1c0e423f492a8b1e23bc555f0d4ce
jairam-43 <jairampadala43@gmail.com>
2025-09-09 06:52:57 +0530

    Initial commit: Add all Dart practice codes

---
+++ 3/tasks.dart
@@ -0,0 +1,11 @@
+
+import 'dart:io';
+import 'dart:math';
+void main()
+{
+    String? a = stdin.readLineSync();
+    String? b = stdin.readLineSync();
+    int i = int.parse(a);
+    int b = int.parse(b);
+    print(i+j);
+}
